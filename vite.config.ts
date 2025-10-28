5e72907bdd8eaff8b089906296a018c1fbd2ff30
Danilo Ramirez <danilram@cisco.com>
2025-10-28 08:26:56 -0600

    Moving documentation to its own folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/zen-ui',
+  base: '/zenui-docs',
   build:{
     sourcemap: true,
     emptyOutDir: true,
